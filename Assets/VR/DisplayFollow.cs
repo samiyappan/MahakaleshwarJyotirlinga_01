1735dde04a03bdf1c48dd2a91d3922c24253f73f
4.2tech <blackyee.m@gmail.com>
2025-05-06 11:58:44 +0530

    6_5

--- Assets/VR/DisplayFollow.cs
+++ Assets/VR/DisplayFollow.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 public class DisplayFollow : MonoBehaviour
 {
+private Vector3 fixedPosition;
 
-    public Transform centerEyeAnchor;   // OVRCameraRig/CenterEyeAnchor
+    void Start()
+    {
+        fixedPosition = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = fixedPosition;
+    }
+   /* public Transform centerEyeAnchor;   // OVRCameraRig/CenterEyeAnchor
     public Transform seatPosition;      // Where the user should stay seated
 
     void LateUpdate()
     {
-        // Only rotation should apply — cancel out position
+        // Only rotation should apply ï¿½ cancel out position
         Vector3 headsetLocalOffset = centerEyeAnchor.localPosition;
         Vector3 cameraOffset = new Vector3(headsetLocalOffset.x, 0, headsetLocalOffset.z);
 
         // Move OVRCameraRig to keep camera at seat position
         transform.position = seatPosition.position - cameraOffset;
-    }
+    }*/
 }
 
 
